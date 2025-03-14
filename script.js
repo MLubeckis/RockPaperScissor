9ea234843d61b07efff6dd56bb1f3ae24b922e08
MLubeckis <185762906+MLubeckis@users.noreply.github.com>
2025-03-14 12:18:57 +0200

    Add para for game output

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-function gameRockPaperScissors() {
+function gameRockPaperScissors(playerInput) {
     
-    function getPlayerChoice(playerChoice) {
-        return playerChoice;
-    }
+    // function getPlayerChoice(playerInput) {
+    //     return playerInput;
+    // }
 
     function getComputerChoice() {
 
@@ -22,33 +22,6 @@ function gameRockPaperScissors() {
         return Math.floor(Math.random() * (max - min + 1) + min);
       }
 
-      //event listener for buttons inputs
-      const rpsChoice = document.querySelector("#rps-choices");
-
-      rpsChoice.addEventListener('click', (event) => {
-        let target = event.target;
-            switch(target.id) {
-                case 'rock':{
-                        return getPlayerChoice(target.id);
-                    break;
-                }
-                case 'papper':{
-                    return getPlayerChoice(target.id);
-                break;
-            }
-                case 'scissors':{
-                    return getPlayerChoice(target.id);
-                break;
-            }
-                default:
-                    break;
-            }
-      })
-
-
-
-
-
       function playRound(playerChoice, computerChoice) {
         let roundWinner
         if (computerChoice == playerChoice) {
@@ -62,33 +35,79 @@ function gameRockPaperScissors() {
 
       let playerWinCount = 0;
       let computerWinCount = 0;
+      let roundCount = 0;
+
+      document.querySelector("#rps-choices").addEventListener('click', (event) => {
+        let target = event.target;
+            switch(target.id) {
+                case 'rock':{
+                    game(target.id);
+                    break;
+                }
+                case 'papper':{
+                    return game(target.id);
+                break;
+            }
+                case 'scissors':{
+                    return game(target.id);
+                break;
+            }
+                default:
+                    break;
+            }
+      })
+
+    const para = document.createElement("p");
+
+
 
       //for (let i = 0; i < 5; i++) {
-        const playerChoice = getPlayerChoice().toLowerCase();
-        const computerChoice = getComputerChoice().toLowerCase();
-        console.log("Player draws: " + playerChoice);
-        console.log("Computer draws: " + computerChoice);
-        let roundWinner = playRound(playerChoice, computerChoice);
-        if (roundWinner == "Player Wins"){
-            playerWinCount = ++playerWinCount;
-        } else if (roundWinner == "Computer Wins") {
-            computerWinCount = ++computerWinCount;
-        }
-        console.log(`${roundWinner}\nPlayer has: ${playerWinCount} wins\nComputer has: ${computerWinCount} wins`);
-      //}
-      let winner = "TIE";
+        function game(playerInput) {
+                //document.querySelector("#score").removeChild(para);
+                para.innerText = "";
+                const playerChoice = playerInput;
+                const computerChoice = getComputerChoice().toLowerCase();
+                para.innerText += "Player draws: " + playerChoice + "\n";
+                para.innerText += "Computer draws: " + computerChoice + "\n";
+                let roundWinner = playRound(playerChoice, computerChoice);
+                if (roundWinner == "Player Wins"){
+                    playerWinCount++;
+                } else if (roundWinner == "Computer Wins") {
+                    computerWinCount++;
+                }
+                para.innerText += `${roundWinner}\nPlayer has: ${playerWinCount} wins\nComputer has: ${computerWinCount} wins\n`;
+                roundCount++;
+            //}
+                let winner = "TIE";
+                if (playerWinCount > computerWinCount){
+                    winner = "Player";
+                  } else if (playerWinCount < computerWinCount){
+                    winner = "Computer";
+                  } 
+                  if (playerWinCount == 5) {
+                    para.innerText += "Player has won the game with 5 points\n";
+                    playerWinCount = 0;
+                    computerWinCount = 0;
+                    roundCount = 0;
+                } else if (computerWinCount == 5){
+                    para.innerText += "Computer has won the game with 5 point\n";
+                    playerWinCount = 0;
+                    computerWinCount = 0;
+                    roundCount = 0;
+                } else {
+                    para.innerText += `After ${roundCount} ${roundCount == 1 ? "round" : "rounds"} the winner is ${winner}\n\n`;
+                }
 
-      if (playerWinCount > computerWinCount){
-        winner = "Player";
-      } else if (playerWinCount < computerWinCount){
-        winner = "Computer";
-      } 
 
-      console.log("The game has ended.\nAfter 5 rounds the winner is:" + winner);
+            }
+            document.querySelector("#score").appendChild(para);
+            
 
     //   console.log(playerChoice);
     //   console.log(computerChoice);
     //   console.log(playRound(playerChoice, computerChoice));
 }
 
-//gameRockPaperScissors();
+
+
+gameRockPaperScissors();
