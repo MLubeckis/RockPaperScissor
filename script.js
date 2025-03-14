be11724ad8705759367dca3af1601f1cb20c4745
MLubeckis <185762906+MLubeckis@users.noreply.github.com>
2025-03-14 09:49:08 +0200

    Rock Paper Scissors

---
+++ script.js
@@ -0,0 +1,72 @@
+function gameRockPaperScissors() {
+    
+    function getPlayerChoice() {
+        let playerChoice;
+        return playerChoice = prompt("Enter: Rock, Paper or Scissors", "Rock");
+    }
+
+    function getComputerChoice() {
+
+        let choice = random(1,3);
+        let computerChoice;
+        if (choice == 1) {
+            return computerChoice = "Rock";
+        } else if (choice === 2) {
+            return computerChoice = "Paper";
+        } else {
+            return computerChoice = "Scissors";
+        }
+    }
+    
+        //Function to calculate random number for choice
+    function random(min, max) {  
+        return Math.floor(Math.random() * (max - min + 1) + min);
+      }
+
+
+
+
+
+      function playRound(playerChoice, computerChoice) {
+        let roundWinner
+        if (computerChoice == playerChoice) {
+            return roundWinner = "TIE";
+        } else if (((playerChoice == "rock")&&(computerChoice == "scissors")) || ((playerChoice == "paper")&&(computerChoice == "rock")) || ((playerChoice == "scissors")&&(computerChoice == "paper"))) {
+            return roundWinner = "Player Wins";
+        } else {
+            return roundWinner = "Computer Wins";
+        }
+      }
+
+      let playerWinCount = 0;
+      let computerWinCount = 0;
+
+      for (let i = 0; i < 5; i++) {
+        const playerChoice = getPlayerChoice().toLowerCase();
+        const computerChoice = getComputerChoice().toLowerCase();
+        console.log("Player draws: " + playerChoice);
+        console.log("Computer draws: " + computerChoice);
+        let roundWinner = playRound(playerChoice, computerChoice);
+        if (roundWinner == "Player Wins"){
+            playerWinCount = ++playerWinCount;
+        } else if (roundWinner == "Computer Wins") {
+            computerWinCount = ++computerWinCount;
+        }
+        console.log(`${roundWinner}\nPlayer has: ${playerWinCount} wins\nComputer has: ${computerWinCount} wins`);
+      }
+      let winner = "TIE";
+
+      if (playerWinCount > computerWinCount){
+        winner = "Player";
+      } else if (playerWinCount < computerWinCount){
+        winner = "Computer";
+      } 
+
+      console.log("The game has ended.\nAfter 5 rounds the winner is:" + winner);
+
+    //   console.log(playerChoice);
+    //   console.log(computerChoice);
+    //   console.log(playRound(playerChoice, computerChoice));
+}
+
+gameRockPaperScissors();
